b5394c7a7ec1188e2dbcdedbe22f7fddce1781d6
grandjek <grandjek@D101-S08>
2024-12-11 10:40:03 +0100

    WIP: Fixed inside beveling

--- v1/ClientBlazor_v1/Utils/MathUtils.cs
+++ v1/ClientBlazor_v1/Utils/MathUtils.cs
@@ -13,32 +13,32 @@
         {
             Vector2D toLeft = left - point;
             Vector2D toRight = right - point;
+
+            double theta = Math.Atan2(toLeft.x * toRight.y - toLeft.y * toRight.x, toLeft.x * toRight.x + toLeft.y * toRight.y);
+            if (theta > Math.PI) (toLeft, toRight) = (toRight, toLeft);
+
             double radius = Math.Min(maxRadius, Math.Min(toLeft.Length, toRight.Length));
             toLeft.Normalize();
             toRight.Normalize();
 
+            // TODO: Fix radius && inside
+
+            if (inside)
+                return Bevel(point + toLeft + toRight, point + toLeft, point + toRight, vertexCount, maxRadius, false);
+
             double thetaLeft = Math.Atan2(toLeft.y, toLeft.x);
             if (thetaLeft < 0) thetaLeft = Math.PI * 2 + thetaLeft;
-            double thetaStep = -Math.Acos(toLeft.x * toRight.x + toLeft.y * toRight.y) / (vertexCount - 1);
+
+            double thetaStep = theta / (vertexCount - 1);
 
             Vector2D[] vertices = new Vector2D[vertexCount];
             for (int i = 0; i < vertexCount; i++)
             {
-                double theta = thetaLeft + thetaStep * i;
-                if (inside)
-                {
-                    vertices[vertexCount - i - 1] = new(
-                        point.x + Math.Cos(theta + Math.PI) * radius + toLeft.x + toRight.x,
-                        point.y + Math.Sin(theta + Math.PI) * radius + toLeft.y + toRight.y
-                    );
-                }
-                else
-                {
-                    vertices[i] = new(
-                        point.x + Math.Cos(theta) * radius,
-                        point.y + Math.Sin(theta) * radius
-                    );
-                }
+                double i_theta = thetaLeft + thetaStep * i;
+                vertices[i] = new(
+                    point.x + Math.Cos(i_theta) * radius,
+                    point.y + Math.Sin(i_theta) * radius
+                );
             }
 
             return vertices;
