8ed20406f916cf848c365a6300089ac43ddbb2a0
KevinGr19 <kevingr19dev@gmail.com>
2025-01-17 02:19:47 +0100

    Added Toast Errors on CRUD operations (nightmare to add)

--- v1/ClientBlazor_v1/Models/RoomType.cs
+++ v1/ClientBlazor_v1/Models/RoomType.cs
@@ -7,8 +7,8 @@ namespace ClientBlazor_v1.Models
     {
         public int Id { get; set; }
 
-        //[Required(ErrorMessage = "Le nom du type de salle est requis")]
-        //[StringLength(30, ErrorMessage = "Le nom du type de salle ne doit pas dépasser 30 caractères")]
+        [Required(ErrorMessage = "Le nom du type de salle est requis")]
+        [StringLength(30, ErrorMessage = "Le nom du type de salle ne doit pas dépasser 30 caractères")]
         public string Name { get; set; }
 
         public List<Room> Rooms { get; set; } = new();
